a18ad0aef8847c3051360eb0005e801a685675a8
Pinnoche <ptime552@gmail.com>
2024-10-21 11:40:30 +0100

    about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This Page will be available soon!!!</h1>
   </div>
 </template>
 
 <style>
+.about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: brown;
+  }
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
+    justify-content: center;
+    color: brown;
   }
 }
 </style>
